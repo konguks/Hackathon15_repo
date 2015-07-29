0bfdd879b6161f8ef29bce5483f9ede878c34d90
konguks <konguskarthick@gmail.com>
2015-07-29 03:38:46 -0700

    latest

--- try_1.php
+++ try_1.php
@@ -2,9 +2,10 @@
 
 class test
 {
-	public function test_fn(){		
-		echo "The total ";
-		
+	public function test_fn($num1,$num2){
+		$tot=$num1+$num2;
+		echo "The total ".$tot;
+		return $tot;
 	}	
 }
 
