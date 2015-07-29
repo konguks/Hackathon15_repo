78b9d79ab41ac50a1b9c73d790479e71f0f84679
konguks <konguskarthick@gmail.com>
2015-07-29 02:55:53 -0700

    new test

--- try_1.php
+++ try_1.php
@@ -1,6 +1,14 @@
 <?php
-$num1 = 10;
-$num2 = 20;
-$tot = $num1+$num2;
-echo "The total ".$tot;
+
+class test
+{
+	public function test_fn(){
+		$num1 = 10;
+		$num2 = 20;
+		$tot = $num1+$num2;
+		echo "The total ".$tot;
+		return $tot;
+	}	
+}
+
 ?>
