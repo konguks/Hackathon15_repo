b2c27234c604468f40819c29cc212715095c9633
konguks <konguskarthick@gmail.com>
2015-07-29 05:56:59 -0700

    chk

--- try_1.php
+++ try_1.php
@@ -2,6 +2,13 @@
 
 class try_1
 {
+	private $pdo;
+	
+	public function __construct(PDO $pdo)
+    {
+        $this->pdo = $pdo;
+    }
+	
 	public function test_fn($num1,$num2){
 		$tot=$num1+$num2;
 		echo "The total ".$tot;
